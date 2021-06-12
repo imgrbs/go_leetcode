3b182a7f27a81b0b63dd16f9fe18bfbd2c15c993
Imagine Rabbits <keerati.jear@gmail.com>
2021-06-12 20:56:18 +0700

    add int to roman with tests

---
+++ roman/roman/int_roman.go
@@ -0,0 +1,55 @@
+package roman
+
+const MinimumValue = 0
+
+var numberToRoman = make(map[int]string)
+var romanKeys = []int{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1}
+
+func initRoman() {
+	numberToRoman[1000] = "M"
+	numberToRoman[900] = "CM"
+	numberToRoman[500] = "D"
+	numberToRoman[400] = "CD"
+	numberToRoman[100] = "C"
+	numberToRoman[90] = "XC"
+	numberToRoman[50] = "L"
+	numberToRoman[40] = "XL"
+	numberToRoman[10] = "X"
+	numberToRoman[9] = "IX"
+	numberToRoman[5] = "V"
+	numberToRoman[4] = "IV"
+	numberToRoman[1] = "I"
+}
+
+func buildRoman(number *int, romanKey int, roman *string) {
+	if isValidRomanKey(number, romanKey) {
+		*number -= romanKey
+		*roman += numberToRoman[romanKey]
+	}
+
+	if isSameRomanKey(number, romanKey) {
+		buildRoman(number, romanKey, roman)
+	}
+}
+
+func isSameRomanKey(number *int, romanKey int) bool {
+	return *number >= romanKey
+}
+
+func isValidRomanKey(number *int, romanKey int) bool {
+	return *number-romanKey >= MinimumValue
+}
+
+func IntToRoman(number int) string {
+	initRoman()
+
+	roman := ""
+
+	for number > 0 {
+		for _, romanKey := range romanKeys {
+			buildRoman(&number, romanKey, &roman)
+		}
+	}
+
+	return roman
+}
---
+++ roman/roman/int_roman_test.go
@@ -0,0 +1,105 @@
+package roman_test
+
+import (
+	"leetcode/roman/roman"
+	"testing"
+)
+
+func Test_Give1_WantI(t *testing.T) {
+	input := 1
+	expect := "I"
+
+	result := roman.IntToRoman(input)
+
+	if result != expect {
+		t.Errorf("Failed, assert %d got %s", input, result)
+	}
+}
+
+func Test_Give5_WantV(t *testing.T) {
+	input := 5
+	expect := "V"
+
+	result := roman.IntToRoman(input)
+
+	if result != expect {
+		t.Errorf("Failed, assert %d got %s", input, result)
+	}
+}
+
+func Test_Give4_WantIV(t *testing.T) {
+	input := 4
+	expect := "IV"
+
+	result := roman.IntToRoman(input)
+
+	if result != expect {
+		t.Errorf("Failed, assert %d got %s", input, result)
+	}
+}
+
+func Test_Give9_WantIX(t *testing.T) {
+	input := 9
+	expect := "IX"
+
+	result := roman.IntToRoman(input)
+
+	if result != expect {
+		t.Errorf("Failed, assert %d got %s", input, result)
+	}
+}
+
+func Test_Give3_WantIII(t *testing.T) {
+	input := 3
+	expect := "III"
+
+	result := roman.IntToRoman(input)
+
+	if result != expect {
+		t.Errorf("Failed, assert %d got %s", input, result)
+	}
+}
+
+func Test_Give20_WantXX(t *testing.T) {
+	input := 20
+	expect := "XX"
+
+	result := roman.IntToRoman(input)
+
+	if result != expect {
+		t.Errorf("Failed, assert %d got %s", input, result)
+	}
+}
+
+func Test_Give21_WantXXI(t *testing.T) {
+	input := 21
+	expect := "XXI"
+
+	result := roman.IntToRoman(input)
+
+	if result != expect {
+		t.Errorf("Failed, assert %d got %s", input, result)
+	}
+}
+
+func Test_Give58_WantLVIII(t *testing.T) {
+	input := 58
+	expect := "LVIII"
+
+	result := roman.IntToRoman(input)
+
+	if result != expect {
+		t.Errorf("Failed, assert %d got %s", input, result)
+	}
+}
+
+func Test_Give1994_WantMCMXCIV(t *testing.T) {
+	input := 1994
+	expect := "MCMXCIV"
+
+	result := roman.IntToRoman(input)
+
+	if result != expect {
+		t.Errorf("Failed, assert %d got %s", input, result)
+	}
+}
